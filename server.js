685cbaf56c5e80b77bafc821257f6a89a2d6c0f4
Graydon Power <graydonmpower@gmail.com>
2024-02-20 21:49:42 -0500

    color change

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 8080;
 
 const playerProperties = {
     ids: [1, 2, 3, 4, 5],
-    colors: ["lightblue", "red", "green", "purple", "yellow"] // Add more colors for additional players
+    colors: ["#ADD8E6", "red", "green", "purple", "yellow"] // Add more colors for additional players
 };
 
 const gameRooms = {};
